fb6c7fb97c29e662301da66c1bf1433df132630d
huxf <zivlhoo@gmail.com>
2014-11-12 11:24:28 +0800

    1.修改项目，在主项目中引用iap项目文件

--- Classes/IAPScene.cpp
+++ Classes/IAPScene.cpp
@@ -1,6 +1,9 @@
 #include "IAPScene.h"
-#include "AppMacros.h"
 
+IAPScene::~IAPScene()
+{
+    
+}
 
 CCScene* IAPScene::scene()
 {
@@ -9,10 +12,10 @@ CCScene* IAPScene::scene()
     
     // 'layer' is an autorelease object
     IAPScene *layer = IAPScene::create();
-
+    
     // add layer as a child to scene
     scene->addChild(layer);
-
+    
     // return the scene
     return scene;
 }
@@ -20,16 +23,75 @@ CCScene* IAPScene::scene()
 // on "init" you need to initialize your instance
 bool IAPScene::init()
 {
-
+    
     if ( !CCLayer::init() )
-    {
         return false;
-    }
     
     CCControlButton* btn = CCControlButton::create("商店", "Heiti SC",30);
     btn->setPosition(ccp(2480/2, 1536/2));
-//    btn->addTargetWithActionForControlEvents(this, cccontrol_selector(HelloWorld::switchView), CCControlEventTouchDown);
+    btn->addTargetWithActionForControlEvents(this, cccontrol_selector(IAPScene::store), CCControlEventTouchDown);
     addChild(btn);
     
+    _iapHelper = IAPHelper::getInstance(*this);
+    
     return true;
+    
+}
+
+void IAPScene::store(CCObject*, CCControlEvent)
+{
+    _iapHelper->requestProducts();
+}
+
+void IAPScene::productsHandle(vector<Product> products)
+{
+    _products = products;
+    Widget* widget = GUIReader::shareReader()->widgetFromJsonFile("ItemProduct.ExportJson");
+    widget->setAnchorPoint(ccp(.5, .5));
+    
+    UILayer* layer = UILayer::create();
+    float x = widget->getContentSize().width/2;
+    float y = widget->getContentSize().height/2 +20;
+    for (int i = 0; i < products.size(); ++i)
+    {
+        Widget* product = widget->clone();
+        
+        Product p = products[i];
+        
+        ImageView* img = static_cast<ImageView*>(product->getChildByName("Image_product"));
+        Label* title = static_cast<Label*>(product->getChildByName("Label_title"));
+        Label* price = static_cast<Label*>(product->getChildByName("Label_price"));
+        Button* buy = static_cast<Button*>(product->getChildByName("Button_buy"));
+        
+        title->setText(p.title);
+        price->setText(p.price);
+        buy->setTag(p.identifier);
+        buy->addTouchEventListener(this, toucheventselector(IAPScene::buy));
+        
+        product->setPosition(ccp(x, y));
+        x += widget->getContentSize().width;
+        
+        layer->addWidget(product);
+    }
+    addChild(layer);
+}
+
+void IAPScene::purchased()
+{
+    CCLog("ccccc purchased");
+}
+
+void IAPScene::purchaseFailed()
+{
+    CCLog("ccccc purchaseFailed");
+}
+
+void IAPScene::buy(CCObject* obj,TouchEventType eventType)
+{
+    if (TOUCH_EVENT_ENDED == eventType)
+    {
+        CCNode* node = static_cast<CCNode*>(obj);
+        
+        _iapHelper->buyProductById(node->getTag());
+    }
 }
--- Classes/IAPScene.h
+++ Classes/IAPScene.h
@@ -3,24 +3,40 @@
 
 #include "cocos2d.h"
 #include "cocos-ext.h"
+#include "IAP.framework/Headers/IAPHelper.h"
+#include "IAP.framework/Headers/Product.h"
 
 USING_NS_CC;
 USING_NS_CC_EXT;
 using namespace ui;
+using namespace std;
 
-class IAPScene : public cocos2d::CCLayer
+class IAPScene : public CCLayer , IAPDelegate
 {
+public:
+    ~IAPScene();
+    
 public:
 
     virtual bool init();  
 
-
     static cocos2d::CCScene* scene();
     
-    void stor(CCObject*, CCControlEvent);
+    void store(CCObject*, CCControlEvent);
+
+    void productsHandle(std::vector<Product>);
+    void purchased();
+    void purchaseFailed();
     
-    // implement the "static node()" method manually
+    void buy(CCObject*,TouchEventType);
+    
     CREATE_FUNC(IAPScene);
+    
+private:
+    
+    IAPHelper       *_iapHelper;
+    vector<Product> _products;
+    
 };
 
 #endif // __HELLOWORLD_SCENE_H__
